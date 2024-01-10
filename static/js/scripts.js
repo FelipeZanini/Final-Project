1f97770d2dc29e4ce307ab6676477e6f1c548eb7
Felipe Zanini <126201980+FelipeZanini@users.noreply.github.com>
2024-01-10 23:11:11 +0000

    Complete home page design

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,20 +2,19 @@ window.addEventListener('DOMContentLoaded', event => {
     $('#search-icon').on('click', () => {
         $('#search-bar').removeClass('d-none');
         $('#search-icon').addClass('d-none');
-    });  
-    $('html').click(function(e){
+    });
+    $('html').click(function (e) {
         var elem = e.target;
-        if ( $(elem).attr('id') == "search-icon" ){
+        if ($(elem).attr('id') == "search-icon") {
             $('#search-bar').removeClass('d-none');
             $('#search-icon').addClass('d-none');
             console.log(elem);
-        }else if ( $(elem).parent().parent().parent().attr('id') == "search-bar" ){
+        } else if ($(elem).parent().parent().parent().attr('id') == "search-bar") {
             console.log("uhdauhda");
-        }else {
+        } else {
             $('#search-bar').addClass('d-none');
             $('#search-icon').removeClass('d-none');
             console.log(elem);
         }
-        });
-  
-});
+    });
+    });
