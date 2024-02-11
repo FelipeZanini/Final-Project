5a6d1e5a70466d78e2943a64d55621dce65327db
Felipe Zanini <126201980+FelipeZanini@users.noreply.github.com>
2024-02-11 16:39:44 +0000

    Created order, order item and shipping address models to manage the cart app

---
+++ cart/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.conf import settings
+from ..products import models
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.date_ordered)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(models.Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
+
+class ShippingAddress(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    county = models.CharField(max_length=200, null=False)
+    eircode = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
