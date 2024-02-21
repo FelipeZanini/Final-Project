5c64fc40b982ea117563babd56d7b477f0dd88e3
Felipe Silva <felipe_zanini@hotmail.com>
2024-02-21 21:33:37 +0000

    Fixed error at contatact us

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import NewsletterSignup
+from .models import ContactUs
 
 # Register your models here.
-admin.site.register(NewsletterSignup)
+admin.site.register(ContactUs)
