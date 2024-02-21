16ce52b0b547029cd1d661ab49c4df7ff739e2c6
Felipe Silva <felipe_zanini@hotmail.com>
2024-02-21 02:48:45 +0000

    Add the database url to envoriment

--- fezin/settings.py
+++ fezin/settings.py
@@ -130,11 +130,8 @@ WSGI_APPLICATION = 'fezin.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
 
 
 # Password validation
