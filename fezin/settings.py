f569ce77afba1cc7202dc1a6ad3e9a2a72979588
Felipe Zanini <126201980+FelipeZanini@users.noreply.github.com>
2024-05-28 23:40:51 +0000

    Set debug to false

--- fezin/settings.py
+++ fezin/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".fly.dev","https://localhost:8000","https://newsapp27.fly.dev", "localhost", "127.0.0.1", "zanfe-009510b2eec0.herokuapp.com",
 ]
@@ -106,21 +106,10 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'fezin.wsgi.application'
 
+
 DATABASES = {
-        'default': dj_database_url.parse('postgres://vfkdlnlb:S6ztu9lD3EY_yU7fmyxCP5_3QjYQMlLv@trumpet.db.elephantsql.com/vfkdlnlb')
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-
-# DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-
-
-# DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.sqlite3',
-#            'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-#             }
-#    }
 
 
 AUTH_PASSWORD_VALIDATORS = [
