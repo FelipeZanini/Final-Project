95f10f1855143e0f95316cca1786e90dd65f11b7
Felipe Zanini <126201980+FelipeZanini@users.noreply.github.com>
2024-06-09 22:55:04 +0000

    Setting debug to false

--- fezin/settings.py
+++ fezin/settings.py
@@ -102,13 +102,8 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'fezin.wsgi.application'
 
 
-# DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
-
-    
 DATABASES = {
-        'default': dj_database_url.parse('postgres://vfkdlnlb:S6ztu9lD3EY_yU7fmyxCP5_3QjYQMlLv@trumpet.db.elephantsql.com/vfkdlnlb')
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 
 
